8e90a00b722b3c553efb99cc39e175e9e35af442
DasGeek <36272016+dasgeekchannel@users.noreply.github.com>
2022-08-13 10:08:34 -0400

    Add files via upload
    Update to add in the elements that control damage, health, etc.

--- wiretapv2.py
+++ wiretapv2.py
@@ -1,16 +1,30 @@
 # coding=utf-8
 # Game and story created by DasGeek
-import climage
+
+# import os
+# os.system('')
+import climage # install climage to convert png for terminal
 import time
-import random #for fight calculations
+import random #for fight calcullations
+import colorama # text color
+from colorama import init, Fore, Back, Style
+# Initializes Colorama
+init(autoreset=True)
 
 
-# install climage to convert png for terminal
 # pip3 install climage dependency
 inventory = []
 gametitle = climage.convert("wiretap.png")
 drone = climage.convert("drone.png")
+city = climage.convert("city_cyberpunk.jpg")
+total_hp = 150
 
+
+
+def death():
+    if total_hp <= 0:
+        print ("You are dead")
+        scene1()
 
 def game():
     import random # for battle calculations
@@ -25,47 +39,82 @@ def game():
         start = False
         print("Ok, some other time")
     if start == True:
-        print("	")
+        print(" ")
         print('Introduction')
+        print(f"Your HP is {total_hp}") #using f string to combine string with numerics
         print(' ')
         print(gametitle)
         print(' ')
-        print(
-            'You step out onto the streets and take a deep breath. Your augment goes to work producing a filtered blast of clean air.')
-        print(
-            'Before you is a city filled with an endless assortment of neon lights and animated holograms force-feeding its message deep into wandering eyes.')
+        input("Press Enter to continue.")
+        print(Fore.RED +"""
+
+            You step out onto the streets and take a deep breath. 
+            Your augment goes to work producing a filtered blast 
+            of clean air.
+
+            Before you is a city filled with an endless assortment
+            of neon lights and animated holograms force-feeding 
+            its message deep into wandering eyes.
+
+            Silhouettes of women dancing alongside augments and 
+            quick stop convenience stores. There are brands of every 
+            type, all promoting themselves with faces of models and 
+            celebrities and influencers begging to be remembered.
+
+            """)
+        
+        print(city)
+        
         print(' ')
-        print(
-            'Silhouettes of women dancing alongside augments and quick stop convenience stores. There are brands of every type, all promoting')
-        print('themselves with faces of models and celebrities and influencers begging to be remembered.')
+        input("Press Enter to continue.")
+        
+        print("""
+            
+            The wet streets and towering buildings reach as far as 
+            the eyes could see. The assortment of colors from the 
+            digital signs bouncing off the grey and black concrete 
+            jungle and their glass patios. An endless barrage of lights 
+            highlighting the filth of an overpopulated city that had 
+            given up on aesthetics long ago.
+
+            """)
+        
         print(' ')
-        time.sleep (1)
-        print(
-            'The wet streets and towering buildings reach as far as the eyes could see. The assortment of colors from the digital signs bouncing off')
-        print(
-            'the grey and black concrete jungle and their glass patios. An endless barrage of lights highlighting the filth of an overpopulated city')
-        print('that had given up on aesthetics long ago.')
-        print(' ')
-        print(
-            'There was an assortment of street vendors and loitering patrons that seem to go on in an endless maze. There are no mountain views,')
-        print(
-            "grass fields, or signs of natural life. You've arrived at Zire. A city once known as a hub for great prosperity but as the population")
-        print(
-            'grew the people built up into the skies until they couldn’t build up any longer, so they started building underground. Signs pointing towards')
-        print(
-            'underground tunnels leading to the armpit of civilization known as The Beneath. Flying vehicles of various shapes and sizes consumed the skies,')
-        print(
-            'swarming like hornets protecting a nest. The whizzing and buzzing blending into the sounds of bustling streets.')
-        print(' ')
-        time.sleep (1)
-        print(
-            'The air is moldy and thick. The planet like a body wrapped in rubber. It takes effort to breathe the industrialized air, especially for those')
-        print('without augmentations.')
-        print(' ')
-        print('Your personal assistant suddenly springs to life in your visor.')
-        print(' ')
-        print('You notice a duffle bag on the ground lying open. Someone must have left it in a hurry.')
-        print(' ')
+        
+        input("Press Enter to continue.")
+        
+        print("""
+            
+            There was an assortment of street vendors and loitering 
+            patrons that seem to go on in an endless maze. There 
+            are no mountain views, grass fields, or signs of natural life. 
+            You've arrived at Zire. A city once known as a hub for great 
+            prosperity but as the population grew the people built up into 
+            the skies until they couldn’t build up any longer, so they started 
+            building underground. Signs pointing towards underground tunnels 
+            leading to the armpit of civilization known as The Beneath. 
+            Flying vehicles of various shapes and sizes consumed the 
+            skies swarming like hornets protecting a nest. The whizzing 
+            and buzzing blending into the sounds of bustling 
+            streets.
+
+        """)
+        
+        input("Press Enter to continue.")
+        
+        print("""
+            The air is moldy and thick. The planet like a body wrapped 
+            in rubber. It takes effort to breathe the 
+            industrialized air, especially for those without 
+            augmentations.
+
+            Your personal assistant suddenly springs to 
+            life in your visor.
+        
+            You notice a duffle bag on the ground lying open. 
+            Someone must have left it in a hurry.
+        
+        """)
         scene1()
 
 
@@ -76,12 +125,18 @@ def scene1():
     ans = "incorrect"
     while ans == "incorrect":
         if c1.lower() == "take":
-            print(
-                "You scrumage inside and find old pair of gym clothes.\n As you keep searching you notice a zipper pocket on the outside of the bag with something heavy inside. \n Upon inspection of the pocket you find a pair of brass knuckles.\n"
-            )
+            print("""
+            
+                "You scrumage inside and find old pair of gym clothes.
+                As you keep searching you notice a zipper pocket on the 
+                outside of the bag with something heavy inside. 
+                Upon inspection of the pocket you find a pair of brass knuckles."
+            
+            """)
             inventory.append("Brass Knuckles")
             print(" ")
             print(str(inventory) + " has been added to your inventory")
+            print(" ")
             ans = "correct"
             scene2()
         elif c1.lower() == "ignore":
@@ -90,7 +145,7 @@ def scene1():
             ans = "correct"
             scene2()
         else:
-            print("ENTER THE CORRECT CHOICE! Lowercase take or ignore?")
+            print("ENTER THE CORRECT CHOICE! Lowercase 'take' or 'ignore?'")
             c1 = input(">>")
 
 
@@ -103,25 +158,76 @@ def scene2():
     ans = "incorrect"
     while ans == "incorrect":
         if c1.lower() == "right":
-            print('You decide to turn right and see a several neon signs and shops in the distance.')
-            print('As you get closer to the shops you see there is general store, a VR World and an') 
-            print('augmentation shop. As you get near the augmentation shop you notice there is an obese') 
-            print('short bald man who seems to being paying special,attention to you as you approach.') 
-            ans = "correct"
+            print("""
+            You decide to turn right and see a several neon signs 
+            and shops in the distance.As you get closer to the shops
+            you see there is general store, a VR World and an 
+            augmentation shop. 
+
+            Your map tells you to keep moving forward toward an alley
+            at the end of the street. 
+
+            As you get near the augmentation shop you notice there is 
+            an obese short bald man who seems to being paying special,
+            attention to you as you approach.
+            
+            You continue past the shop and notice the merchant is still
+            eyeing you closely.As you pass him, you notice he points at 
+            you and whistles.
+            
+            A massive robot grabs your arm and lifts you into the alleyway
+            
+            They push you into an alleyway before you have a a chance 
+            to turn back around. There is a large 
+
+
+            """)
+            input("Press Enter to continue.")
+            ans='correct'
+            c3 = input(">>")
+            while ans == "incorrect":
+                if c3.lower =="talk":
+                    print("this is a test")
+                    scene3()
+                elif c3.lower =="run":
+                    print("this is also a test")
+                    scene3()
             print(str(inventory))
             scene3()
+            ans = 'correct'
         elif c1.lower() == "left":
-            print("You turn to the left and proceed down a dark alleyway. The streets are littered with trash and used needles.\n")
-            print("A nearby buzzing sound enters your ears grabbing your attention. You find the source of the noise as a giant \n drone drops from the sky directly in your path.\n")
-            print(" ")
+            print("""
+            You turn to the left and proceed down a dark alleyway. 
+            The streets are littered with trash and used needles.
+            A nearby buzzing sound enters your ears grabbing your 
+            attention. You find the source of the noise as a giant 
+            drone drops from the sky directly in your path.
+            """)
             input("Press Enter to continue.")
-            print("'Welcome patron, please register.' you notice the drone is a quadcopter with a massive round lens which keeps focusing and refocusing as")
-            print("if impatiently waiting for a response. The drone is badly weathered but the camera seems recently upgraded.\n")
-            print("On the outside of the drone's lens is a ring of light emitting a green'ish hue.\n")
+            print("""
+
+            'Welcome patron, please register.' you notice the drone 
+            is a quadcopter with a massive round lens which keeps 
+            focusing and refocusing as if impatiently waiting for a 
+            response. The drone is badly weathered but the camera 
+            seems recently upgraded. On the outside of the drone's
+            lens is a ring of light emitting a green'ish hue.
+
+            """)
             input("Press Enter to continue.")
-            print("'Welcome citizen, please present bar code.' the drone is a quadcopter with a massive round lens which keeps focusing \n ")
-            print("and refocusing over and over again as if impatiently waiting for a response. The drone had signs of once being brightly colored \n with faded strips of paint that was now badly weathered but \n ")
-            print("the camera seems recently upgraded. On the outside of the drone's lens is a ring of light emitting a red hue.\n")
+            
+            print("""
+            
+            'Welcome citizen, please present bar code.' the drone
+            is a quadcopter with a massive round lens which keeps
+            focusing and refocusing over and over again as if 
+            impatiently waiting for a response. The drone had 
+            signs of once being brightly colored with faded 
+            strips of paint that was now badly weathered but 
+            the camera seems recently upgraded. On the outside 
+            of the drone's lens is a ring of light emitting a red hue.
+            
+            """)
             print(drone)
             input("Press Enter to continue.")
             print("Do you want to 'register'? or 'attempt hack' ? ")
@@ -130,18 +236,33 @@ def scene2():
     ans = "incorrect"
     while ans == "incorrect":
         if c2.lower()== "register":
-            print(" you lift your sleeve revealing a bare arm with no barcode. The drones lens focuses in floats backwards defensively")
-            print(" 'Another recycled war drone' ...you think to yourself ")
-            print("your eyes are fixated on the barrel hanging from the bottom of the drone. ")
+            print(""" 
+
+            you lift your sleeve revealing a bare arm with no barcode. 
+            The drones lens focuses in floats backwards defensively
+            'Another recycled war drone' ...you think to yourself 
+            your eyes are fixated on the barrel hanging from the bottom of the drone.
+
+            """)
+            
             time.sleep(1)
             print("'Yes, register'...you say aloud.' ")
             time.sleep(1)
-            print("'Please present registration chip.' the drone's voice echoed with a synthesized and broken tone. ")
-            print("You lift the sleeve of your shirt revealing a bare arm to the drone's camera.")
-            print("The drone quickly floats backwards a few feet. The gun barrel at the bottom of the drone is pointing directly at you.")
-            print("Red lights illuminate on the drone as it prepares to attack you.")
-            print("'Unregistred citizens are in violation of code 816-3 and are subject to arrest. Please stay where you are.'")
-            print("You can't be arrested again or you'll face life in prison. You know what you have to do.")
+            
+            print("""
+            'Please present registration chip.' the drone's voice echoed 
+            with a synthesized and broken tone. You lift the sleeve of 
+            your shirt revealing a bare arm to the drone's camera.
+            "The drone quickly floats backwards a few feet. The gun 
+            barrel at the bottom of the drone is pointing directly at you.
+            Red lights illuminate on the drone as it prepares to attack you.
+            'Unregistred citizens are in violation of code 816-3 and are 
+            subject to arrest. Please stay where you are.'
+            You can't be arrested again or you'll face life in prison. 
+            You know what you have to do.
+            
+            """)
+            
             input("Press Enter to Continue.")
             print("You currently have this in your inventory:")
             print(inventory)
@@ -155,10 +276,13 @@ def scene2():
 
 # Define Fight 1 Simple Enemy
 def fight1():
+    global total_hp #set to global to allow damage to continue reducing HP. 
+    print(total_hp)
     print("***You Are In A Fight***")
     print("A: punch")
     print("B: kick")
-    print("C: Use Brass Knuckles")
+    if "Brass Knuckles" in inventory:
+        print("C: Use Brass Knuckles")
     decision=input('What action do you choose: A,B, or C? ')
 
     minPoint=1
@@ -167,23 +291,77 @@ def fight1():
     ChA=(random.randint(minPoint,maxPoint))*.85
     ChB=(random.randint(minPoint,maxPoint))*.33
     ChC=(random.randint(minPoint, maxPoint))*.95
+    DMG_ind=(random.randint(minPoint, maxPoint))
 
-    if decision.upper()=='A' and ChA>25:
-        print('you punch the drone')
-        print('the drone shoots you with a tranquilizer dart')
+
+
+    if decision.upper()=='A' and ChA>25 and total_hp >0:
+        print('you punch at the drone but it easily dodges out of the way.')
+        print('You see a dart whiz past your face.')
+        print('Your HUD identifies the object as a tranquilizer dart')
+        print('You might have one more chance to take the drone down.')
+        total_hp=total_hp - DMG_ind
+        print(total_hp)
+        fight1()
+
+    elif total_hp <0:
+        print("you are dead")
+        print(" ")
+        print("Game Over")
+        print(" ")
+        input("Press Enter to continue.")
+        scene2()
 
     elif decision.upper()=='B' and ChB>25:
         print('you kick the drone')
         print('the drone loses balance and crashes into a light pole.')
+        print("You notice a figure standing in the shadows. Your vision goes blurry and you collapse to the ground")
+        scene3()
 
     elif decision.upper()=='C' and ChC>25 and 'Brass Knuckles' in inventory:
         print('you use your brass knuckles')
         print('the punch bursts the glass camera and send the drone into a spin')
+        print("You notice a figure standing in the shadows. Your vision goes blurry and you collapse to the ground")
+        scene3()
     else:
-        print("Your hit didn't work, you're going to jail")
+            print("You regret your poor decision making")
+            fight1()
 
+"""
+Story choices above all lead here eventually.
+
+"""
 def scene3():
-    print("welcome to my store")
+    print("Well, well, you're finally awake.")
+    print("That drone hit you with a tranquilzer dart. What were you thinking attacking a corporate drone with no weapons?")
+    input("Press ENTER to continue")
+    print("You look around and notice a storage room full of metal shelves, boxes overflowing with electronics")
+    print("My name is Uzman. Nice to meet you.")
+    input("Press ENTER to continue")
+    print("As your vision becomes more clear you notice that you're strapped down to a table. ")
+    print("You look down and notice your augments are removed from your arms and legs. There are large open wounds where your augments once resided")
+    print("You see a scalpel covered in blood on the table beside you. ")
+    print("You try to access your HUD but there is no overlay")
+    input("Press ENTER to continue")
+    print("Imagine my surprise when I found you passed out with over a million Eth in augments.")
+    print("Just know it's nothing personal. You don't look a gift horse in the mouth as they say.")
+    print("I could leave you for dead and dump you back where I found you. I'm greedy but I'm not evil, you know")
+    input("Press ENTER to continue")
+    print("You notice you can move your fingers on your right arm. Pain starts to register as you become more awake and the pain sets in.")
+    input("Press ENTER to continue")
+    print("You're not registered with the government...so it's not like they would even investigate your death.")
+    print("")
 
+   # c3 = input(">>")
+   # print ("What would you like to do? A. Fight B. Scream")
+   # ans = "incorrect"
+   # while ans = "incorrect":
+   #     if c1 = "A"
+   #     print 
+
+
+
+
+# either way they wake up in the store from Drone battle or they travel safely to the store going right
 
 game()
